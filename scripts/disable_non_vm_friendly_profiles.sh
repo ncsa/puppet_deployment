20b843b050d898228a30b626f1230530ecd3e848
Andy Loftus <aloftus@gmail.com>
2018-06-02 12:39:46 -0500

    add disable_non_vm_friendly_profiles

---
+++ scripts/disable_non_vm_friendly_profiles.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PUPPET=/opt/puppetlabs/bin/puppet
+ENVDIR=$( $PUPPET config print environmentpath )
+
+set -x
+now=$(date +%s)
+for env in $(ls -d $ENVDIR/*); do
+    manifestdir=$env/modules/role/manifests
+    find $manifestdir -mindepth 1 -maxdepth 1 -name '*.pp' \
+    | while read; do
+        bak=${REPLY}.$now
+        cp $REPLY $bak
+        awk '
+/allow_qualys_scan/ && ! /#/ { print "#",$0; next }
+/gpfs/              && ! /#/ { print "#",$0; next }
+/telegraf/          && ! /#/ { print "#",$0; next }
+/yum_client/        && ! /#/ { print "#",$0; next }
+/slurm/             && ! /#/ { print "#",$0; next }
+{print}
+' $bak > $REPLY
+    done
+done
+
