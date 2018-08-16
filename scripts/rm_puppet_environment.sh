c76660d1304cf288bb053a8e8a8bd84fb37cac8a
Andy Loftus <aloftus@gmail.com>
2018-08-16 09:38:43 -0500

    rm old testing script

--- scripts/rm_puppet_environment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PUPPET=/opt/puppetlabs/bin/puppet
-ENVDIR=$( $PUPPET config print environmentpath )
-
-set -x
-ls $ENVDIR \
-| grep -v 'production\|test' \
-| xargs -r -n1 -I{} find $ENVDIR/{} -delete
